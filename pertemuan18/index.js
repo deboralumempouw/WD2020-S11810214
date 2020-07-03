00baa4b25e3abf2fb6c97f7175cf8610b26dc3be
deboralumempouw <s11810214@student.unklab.ac.id>
2020-07-03 05:56:38 -1200

    menambahkan pertemuan10

---
+++ pertemuan18/index.js
@@ -0,0 +1,50 @@
+console.log("Hello World dari external JS");
+
+//let firstName ="john";
+//let $number = 3;
+//let years;
+//var age = 23;
+//let isMarried = false;
+
+//firstName = "jane";
+
+//console.log(firstName);
+//console.log($number);
+//console.log(isMarried);
+//console.log(years);
+
+//2.Operator JavaScripth
+
+//Operator Aritmatika
+//let num1 = 10;
+//let num2 =2;
+
+//console.log(num1 + num2);
+/*console.log(num1 - num2);
+console.log(num1 * num2);
+console.log(num1 / num2);
+console.log(num1 % num2);
+console.log(num1 ** num2);
+num1++;
+console.log(num1);
+num1--;
+console.log(num1);
+
+//Asigment Operator
+num4=num3;
+console,log(num4)
+let num3 =10;
+let num4;
+
+num4 = num3;
+console.log(num4);
+
+num3 **= 2;
+console.log
+
+
+
+
+
+
+
